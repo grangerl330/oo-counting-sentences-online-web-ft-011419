1ec43476a2db718449e92dccac9a4c3ae6a71273
Luke Granger <grangerl330@gmail.com>
2019-01-22 19:12:03 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,5 +15,5 @@ class String
   def count_sentences
     self.split(/[.?!]\s/).count 
   end
-  
+  #Uses regex to provide an argument of what the .split method should split by. [.?!] means it will split if it sees any one of the characters within the brackers directly followed by a space, which is signified by \s. This will prevent it from splitting when there are 2 or more punctuation marks next to each other. It will only split when the sentence is complete because every sentence ends with a punctuation followed by a space. 
 end
