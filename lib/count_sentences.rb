3d7d9699411a9a7c2c4620ccc38c79ffe92771ea
Luke Granger <grangerl330@gmail.com>
2019-01-22 19:02:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class String
 
   def sentence?
@@ -16,5 +15,5 @@ class String
   def count_sentences
     self.split(/[.?!]\s/).count 
   end
-  #Uses regex to provide an argument of what the .split method should split by. [.?!] means it will split by each of the characters within the brackets and \s means it will split by any whitespace character 
+  
 end
