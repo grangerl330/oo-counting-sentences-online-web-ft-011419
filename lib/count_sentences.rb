410d1292d9051da49890a809df897db0533cc2ac
Luke Granger <grangerl330@gmail.com>
2019-01-22 18:53:36 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    array = self.split(/[.?!]\s/)
+    array = self.split(/[.?!]+/)
   end
   #Uses regex to provide an argument of what the .split method should split by. [.?!] means it will split by each of the characters within the brackets and \s means it will split by any whitespace character 
 end
